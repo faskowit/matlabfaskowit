22d7ec991a6a751364d045846c2262620082b72a
Josh Faskowitz <faskowit@users.noreply.github.com>
2020-12-28 16:59:15 -0500

    Update imsc_grid_comm.m

--- src/viz/imsc_grid_comm.m
+++ src/viz/imsc_grid_comm.m
@@ -1,4 +1,4 @@
-function [h, INDSORT] = imsc_grid_comm(dat,ca,lineWidth,color,names)
+function [h, INDSORT] = imsc_grid_comm(dat,ca,lineWidth,color,offD,names)
 % 
 % dat = your data
 % ca = community affliation of your vector
@@ -15,6 +15,10 @@ if ~exist('color','var') || isempty(color)
    color = [1 0 0] ; 
 end
 
+if ~exist('offD','var') || isempty(offD)
+   offD = 0 ; 
+end
+
 if ~exist('names','var') || isempty(names)
    names = [] ; 
 end
@@ -25,8 +29,7 @@ h = imagesc(dat(INDSORT,INDSORT)) ;
 hold on;  
 plot(X,Y,'Color',color,'linewidth',lineWidth) ;
 hold off;
-
-
+    
 if ~isempty(names)
     
     if length(unique(ca))~=length(names)
@@ -34,12 +37,40 @@ if ~isempty(names)
     end
     
     ax = gca ;
-
-    Y1 =  Y(1:6:end) ;
-    Y2 =  Y(2:6:end) ;
+    Y1 =  Y(1:6:end) ; Y2 =  Y(2:6:end) ;
     Y3 = (Y2+Y1) ./ 2 ;
-
     ax.YTick = Y3 ; 
     ax.YTickLabel = names ;
+end
 
+%% off diagonal lines
+
+if offD
+
+    hold on
+    breaks = X(3:6:end) ;
+
+    % plot off diagonal
+    for idx = 1:(length(breaks)-1)
+
+        lineWidth = 0.5;
+        %offDiagColor = [1 0 0 0.25] ; 
+        offDiagColor = [1 1 1 0.55] ; 
+
+        % vertical   
+        plot([breaks(idx),breaks(idx)],[breaks(idx+1),breaks(end)],...
+            'Color',offDiagColor,'LineWidth',lineWidth);
+        if idx > 1
+            plot([breaks(idx),breaks(idx)],[-0.5,breaks(idx-1)],...
+                'Color',offDiagColor,'LineWidth',lineWidth);
+        end
+        % horizontal
+        plot([breaks(idx+1),breaks(end)],[breaks(idx),breaks(idx)],...
+            'Color',offDiagColor,'LineWidth',lineWidth);
+        if idx > 1
+            plot([-.5,breaks(idx-1)],[breaks(idx),breaks(idx)],...
+                'Color',offDiagColor,'LineWidth',lineWidth);
+        end
+    end  
+    hold off
 end
