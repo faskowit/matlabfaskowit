434be6e8e344c50b10ac5a3fbb97e13affcd7853
josh faskowitz <joshua.faskowitz@gmail.com>
2021-08-27 14:29:33 -0400

    added a lil piece of example data

--- src/netpd/netpd_edgevalbins.m
+++
@@ -1,9 +0,0 @@
-function [edge_bins] = netpd_edgevalbins(D1,D2) 
-% get reasonable edge_bins for two distance matricies
-
-v1 = D1(~eye(size(D1))) ;
-v2 = D2(~eye(size(D2))) ;
-
-combo = unique([v1 ; v2]) ;
-
-[~,edge_bins] = histcounts(combo,'BinMethod','sturges') ;
