63e4df7118a96bf84844a706f8ee3bd7174b0482
josh faskowitz <joshua.faskowitz@gmail.com>
2022-10-06 11:32:42 -0400

    make square better

--- src/utils/make_square.m
+++ src/utils/make_square.m
@@ -1,14 +1,12 @@
-function [ a , aFull ] = make_square(M,K)
+function [ a ] = make_square(M,K)
 % make triu + diag outta vect
 
 if nargin < 2 
-   K = 0 ; 
+   K = 1 ; 
 end
 
 b = length(M) ;
 s = sqrt(2 * b + 0.25) - 0.5 + K;
 a = triu(ones(s),K) ;
-a(~~a) = M ;
-if nargout > 1
-    aFull = a + triu(a,1)';
-end
+a(logical(a)) = M ;
+a = a + triu(a,K)' ; 
